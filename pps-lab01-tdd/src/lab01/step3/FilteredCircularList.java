05a7ea05886344504b09f2f23cf1e0b0c981e264
Alex Siroli <alex.siroli@studio.unibo.it>
2023-03-04 03:08:52 +0100

    bug fix

--- pps-lab01-tdd/src/lab01/step3/FilteredCircularList.java
+++ pps-lab01-tdd/src/lab01/step3/FilteredCircularList.java
@@ -1,11 +1,10 @@
 package lab01.step3;
 
-import java.util.Iterator;
 import java.util.Optional;
 import java.util.function.Predicate;
 
 /**
- * Represents a list of integers, with a built-in iterator that is bidirectional and circular.
+ * Represents a list of integers, with a built-in filtered iterator that is bidirectional and circular.
  * Example: with a list like {1,2,3}, the first call of next() returns 1, the second call returns 2,
  * the third returns 3, the fourth returns 1, and so on. Call to previous() goes in the other way.
  */
